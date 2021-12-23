fac499131b06592dd68789f8afd158dba19cf6a6
sokolan.vv <sokolan.vv@gmail.com>
2021-12-23 19:37:10 +1000

    More test for assignment

--- tests/test7.js
+++ tests/test7.js
@@ -21,4 +21,106 @@ module.exports = test => {
       }
     ]
   })
+
+  test(`
+    x = y = 123;
+    x += y;
+  `, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: { 
+          type: 'AssignmentExpression',
+          operator: 'ASSIGN',
+          left: {
+            type: 'Identifier',
+            value: 'x'
+          },
+          right: {
+            type: 'AssignmentExpression',
+            operator: 'ASSIGN',
+            left: {
+              type: 'Identifier',
+              value: 'y'
+            },
+            right: {
+              type: 'NumericLiteral',
+              value: 123
+            }
+          }
+        }
+      },
+      {
+        type: 'ExpressionStatement',
+        expression: {
+          type: 'AssignmentExpression',
+          operator: 'ASSIGN_ADD',
+          left: {
+            type: 'Identifier',
+            value: 'x'
+          },
+          right: {
+            type: 'Identifier',
+            value: 'y'
+          }
+        }
+      }
+    ]
+  })
+
+  test(`
+    x -= 10;
+    x *= 10;
+    x \\= 10;
+  `, {
+    type: 'Program',
+    body: [
+      {
+        type: 'ExpressionStatement',
+        expression: { 
+          type: 'AssignmentExpression',
+          operator: 'ASSIGN_REM',
+          left: {
+            type: 'Identifier',
+            value: 'x'
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 10
+          }
+        }
+      },
+      {
+        type: 'ExpressionStatement',
+        expression: { 
+          type: 'AssignmentExpression',
+          operator: 'ASSIGN_MUL',
+          left: {
+            type: 'Identifier',
+            value: 'x'
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 10
+          }
+        }
+      },
+      {
+        type: 'ExpressionStatement',
+        expression: { 
+          type: 'AssignmentExpression',
+          operator: 'ASSIGN_DIV',
+          left: {
+            type: 'Identifier',
+            value: 'x'
+          },
+          right: {
+            type: 'NumericLiteral',
+            value: 10
+          }
+        }
+      }
+    ]
+  })
 }
